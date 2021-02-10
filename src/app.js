9e32e79a1973cbe227663739efb80606f0181814
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-02-10 14:07:41 -0500

    Creating controllers,models and routes to Project and Task

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import express from 'express'
+import express, {json,urlencoded} from 'express'
 import morgan from 'morgan'
+import projectRoute from './routes/project.routes'
+import taskRoute from './routes/task.routes'
 
 const app = express()
 
 app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(json())
+app.use(urlencoded({extended: false}))
+
+
+app.use('/api/project', projectRoute)
+app.use('/api/task', taskRoute)
 
 app.get('/', (req,res)=>{
   res.json({
---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,1 @@
+import Task from '../models/Task'
