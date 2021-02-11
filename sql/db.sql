edead033b33ef57046c1ca270d289ad78e9f18da
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-02-10 19:32:15 -0500

    Creating our database with PostgreSQL

---
+++ sql/db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS projects(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL CHECK (name <> ''),
+  priority INTEGER NOT NULL,
+  description TEXT,
+  deliveryday DATE NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS tasks(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL CHECK (name <> ''),
+  done BOOLEAN,
+  projectid INTEGER REFERENCES projects(id)
+);
+
+INSERT INTO projects(name,priority,description,deliveryday) VALUES('Make a web app', 1, 'Using Angular framework', '2021-02-25');
+INSERT INTO projects(name,priority,description,deliveryday) VALUES('Create a mobile app', 2, 'Using Flutter', '2021-03-05');
+INSERT INTO projects(name,priority,description,deliveryday) VALUES('Create a Rest Api', 2, 'Using Laravel', '2021-03-16');
+INSERT INTO projects(name,priority,description,deliveryday) VALUES('Create a console app', 1, 'Using C#', '2021-03-28');
+INSERT INTO projects(name,priority,description,deliveryday) VALUES('Make a desktop app', 1, 'Using Java', '2021-04-12');
+
+INSERT INTO tasks(name,done,projectid) VALUES('Download Angular cli', true, 1);
+INSERT INTO tasks(name,done,projectid) VALUES('Download Android Studio', true, 2);
+INSERT INTO tasks(name,done,projectid) VALUES('Download Postman', true, 3);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import app from "./app";
 import config from './config/keys'
 
-app.listen(config.port, ()=>{
+
+async function main() {
+  await app.listen(config.port, ()=>{
   console.log(`Server running in port: ${config.port}`)
 })
+}
+
+main()
