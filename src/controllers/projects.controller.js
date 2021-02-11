e78f36f010504acd1b037641e1727b4ee635b9ea
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-02-11 10:40:50 -0500

    Creating project controller and add save method

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -1,1 +1,30 @@
 import Project from '../models/Project'
+
+export const saveProject = async (req,res)=>{
+  const {name,priority,description,deliveryday} = req.body
+
+  try {
+    const newProject = await Project.create({
+      name,
+      priority,
+      description,
+      deliveryday
+    }, {
+      fields: ['name','priority','description','deliveryday']
+    })
+  
+    if (newProject) {
+      return res.json({
+        msg: "Project saved successfully!",
+        data: newProject
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      msg: "Something goes wrong!",
+      data: {}
+    })
+  }
+
+}
--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { saveProject } from "../controllers/projects.controller";
 
 const router = Router()
+
+router.post('/', saveProject)
 
 export default router
